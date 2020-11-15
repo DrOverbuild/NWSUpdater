8e6a9fcea1fa65648871ed549d5d71ae65bcf132
Jasper Reddin <jasper@jasperreddin.com>
2020-11-14 21:00:04 -0600

    Implement createAccount, and getUser

--- src/main/java/com/aca/nwsupdater/dao/NWSUpdaterDAOImpl.java
+++ src/main/java/com/aca/nwsupdater/dao/NWSUpdaterDAOImpl.java
@@ -15,6 +15,17 @@ public class NWSUpdaterDAOImpl implements NWSUpdaterDAO{
 			"SELECT id, email, phone FROM user " +
 					"WHERE email = ? AND password = AES_ENCRYPT(?, ?)";
 
+	private static final String createNewUser =
+			"INSERT INTO user (email, phone, password) " +
+					"VALUES (?, ?, AES_ENCRYPT(?, ?))";
+
+	private static final String selectUserById =
+			"SELECT id, email, phone FROM user " +
+					"WHERE id = ?";
+
+	private static String selectId =
+			"SELECT LAST_INSERT_ID() AS 'id'";
+
 	/**
 	 * Authenticates the user with given log in information.
 	 * @param loginInformation
@@ -37,13 +48,12 @@ public class NWSUpdaterDAOImpl implements NWSUpdaterDAO{
 			rs = stmt.executeQuery();
 
 			while (rs.next()) {
-				loggedInUser = new User();
-				loggedInUser.setId(rs.getInt("id"));
-				loggedInUser.setEmail(rs.getString("email"));
-				loggedInUser.setPhone(rs.getString("phone"));
+				loggedInUser = makeUser(rs);
 			}
 		} catch (SQLException throwables) {
 			throwables.printStackTrace();
+		} finally {
+			closeResourses(rs, stmt, conn);
 		}
 
 		return loggedInUser;
@@ -51,12 +61,55 @@ public class NWSUpdaterDAOImpl implements NWSUpdaterDAO{
 
 	@Override
 	public User createAccount(User information) {
-		return null;
+		User newUser = null;
+
+		Connection conn = NWSUpdaterDB.getConnection();
+		PreparedStatement stmt = null;
+
+		try {
+			stmt = conn.prepareStatement(createNewUser);
+			stmt.setString(1, information.getEmail());
+			stmt.setString(2, information.getPhone());
+			stmt.setString(3, information.getPassword());
+			stmt.setString(4, information.getPassword().substring(0,1));
+			int rowsUpdated = stmt.executeUpdate();
+
+			if (rowsUpdated == 1) {
+				int id = getLastInsertedID(conn);
+				newUser = getUser(id);
+			}
+		} catch (SQLException throwables) {
+			throwables.printStackTrace();
+		} finally {
+			closeResourses(null, stmt, conn);
+		}
+
+		return newUser;
 	}
 
 	@Override
 	public User getUser(int userID) {
-		return null;
+		User user = null;
+
+		Connection conn = NWSUpdaterDB.getConnection();
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+
+		try {
+			stmt = conn.prepareStatement(authenticateQuery);
+
+			rs = stmt.executeQuery();
+
+			while (rs.next()) {
+				user = makeUser(rs);
+			}
+		} catch (SQLException throwables) {
+			throwables.printStackTrace();
+		} finally {
+			closeResourses(rs, stmt, conn);
+		}
+
+		return user;
 	}
 
 	@Override
@@ -87,5 +140,54 @@ public class NWSUpdaterDAOImpl implements NWSUpdaterDAO{
 	@Override
 	public List<Alert> getAlerts() {
 		return null;
+	}
+
+	private int getLastInsertedID(Connection conn) {
+		ResultSet rs = null;
+		PreparedStatement stmt = null;
+
+		int id = -1;
+
+		try {
+			stmt = conn.prepareStatement(selectId);
+			rs = stmt.executeQuery();
+
+			while (rs.next()) {
+				id = rs.getInt("id");
+			}
+		} catch (SQLException throwables) {
+			throwables.printStackTrace();
+		} finally {
+			closeResourses(rs, stmt, null);
+		}
+
+		return id;
+	}
+
+	private User makeUser(ResultSet rs) throws SQLException {
+		User user = new User();
+		user.setId(rs.getInt("id"));
+		user.setEmail(rs.getString("email"));
+		user.setPhone(rs.getString("phone"));
+
+		return user;
+	}
+
+	private void closeResourses(ResultSet rs, PreparedStatement stmt, Connection conn) {
+		try {
+			if (stmt != null) {
+				rs.close();
+			}
+
+			if (rs != null) {
+				stmt.close();
+			}
+
+			if (conn != null) {
+				conn.close();
+			}
+		} catch (SQLException throwables) {
+			throwables.printStackTrace();
+		}
 	}
 }
