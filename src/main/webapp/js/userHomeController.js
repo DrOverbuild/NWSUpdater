3104860d2c88f4ef1560bfa92932e9cdd8a47a3b
Jasper Reddin <jasper@jasperreddin.com>
2020-12-11 01:53:05 -0600

    Hook up models with view

--- src/main/webapp/js/userHomeController.js
+++ src/main/webapp/js/userHomeController.js
@@ -2,45 +2,8 @@
     var nwsapp = angular.module('nwsupdaterapp');
 
     nwsapp.controller('userHomeController', function ($scope, $http, $location, $sessionStorage) {
-        $scope.username = "jreddin1@cub.uca.edu";
-        $scope.locations = [
-            {
-                emailEnabled: false,
-                id: 1,
-                lat: 0.0,
-                lon: 0.0,
-                name: "Little Rock",
-                ownerID: 1,
-                smsEnabled: true
-            },
-            {
-                emailEnabled: true,
-                id: 2,
-                lat: 0.0,
-                lon: 0.0,
-                name: "Miami Florida",
-                ownerID: 1,
-                smsEnabled: true
-            },
-            {
-                emailEnabled: true,
-                id: 3,
-                lat: 0.0,
-                lon: 0.0,
-                name: "Conway, AR",
-                ownerID: 1,
-                smsEnabled: true
-            },
-            {
-                emailEnabled: true,
-                id: 10,
-                lat: 28.541047,
-                lon: -81.385516,
-                name: "Orlando",
-                ownerID: 1,
-                smsEnabled: true
-            }
-        ];
+        $scope.userEemail = 'Logging in...';
+        $scope.locations = [];
 
         $scope.getUserHome = function () {
             const sessionID = $sessionStorage.get('sessionID')
@@ -49,8 +12,8 @@
 
                 $http.get("/NWSUpdater/webapi").then(
                     function (response) {
-                        console.log("Success");
-                        // todo load locations
+                        $scope.userEmail = response.data.user.email;
+                        $scope.locations = response.data.locations;
                     }, function (error) {
                         console.log(error);
                         $location.path("/login");
@@ -70,6 +33,6 @@
             // TODO implement
         }
 
-        // $scope.getUserHome();
+        $scope.getUserHome();
     });
 })()
