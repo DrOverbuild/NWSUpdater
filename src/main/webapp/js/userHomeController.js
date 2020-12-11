9746c5971da683bdeb1e45000095be46f13aa251
Jasper Reddin <jasper@jasperreddin.com>
2020-12-10 21:12:33 -0600

    Build table for viewing locations

--- src/main/webapp/js/userHomeController.js
+++ src/main/webapp/js/userHomeController.js
@@ -2,6 +2,46 @@
     var nwsapp = angular.module('nwsupdaterapp');
 
     nwsapp.controller('userHomeController', function ($scope, $http, $location, $sessionStorage) {
+        $scope.username = "jreddin1@cub.uca.edu";
+        $scope.locations = [
+            {
+                emailEnabled: false,
+                id: 1,
+                lat: 0.0,
+                lon: 0.0,
+                name: "Little Rock",
+                ownerID: 1,
+                smsEnabled: true
+            },
+            {
+                emailEnabled: true,
+                id: 2,
+                lat: 0.0,
+                lon: 0.0,
+                name: "Miami Florida",
+                ownerID: 1,
+                smsEnabled: true
+            },
+            {
+                emailEnabled: true,
+                id: 3,
+                lat: 0.0,
+                lon: 0.0,
+                name: "Conway, AR",
+                ownerID: 1,
+                smsEnabled: true
+            },
+            {
+                emailEnabled: true,
+                id: 10,
+                lat: 28.541047,
+                lon: -81.385516,
+                name: "Orlando",
+                ownerID: 1,
+                smsEnabled: true
+            }
+        ];
+
         $scope.getUserHome = function () {
             const sessionID = $sessionStorage.get('sessionID')
             if (sessionID) {
@@ -22,6 +62,14 @@
 
         }
 
-        $scope.getUserHome();
+        $scope.viewLocation = function (locId) {
+            // TODO implement
+        }
+
+        $scope.newLocation = function() {
+            // TODO implement
+        }
+
+        // $scope.getUserHome();
     });
 })()
