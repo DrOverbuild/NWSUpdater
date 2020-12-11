f76a37cdbcc1e18a95f04c7625fa2c86a5383728
Jasper Reddin <jasper@jasperreddin.com>
2020-12-11 02:32:39 -0600

    Improve page load and widen create account

--- src/main/webapp/js/homeController.js
+++ src/main/webapp/js/homeController.js
@@ -7,27 +7,10 @@
         $scope.getHome = function () {
             const sessionID = $sessionStorage.get('sessionID');
             if (sessionID) {
-                $http.defaults.headers.common.Authorization = `Bearer ${sessionID}`;
+                $location.path('/userhome');
+            } else {
+                $location.path(`/login`);
             }
-
-            $http.get("/NWSUpdater/webapi/")
-                .then(function (response) {
-                        $scope.msg = "Logged in"
-                        $location.path('/userhome');
-                    },
-                    function error(response) {
-                        console.log('Error')
-                        console.log(response)
-                        $sessionStorage.put('code', response.data.code);
-
-                        if (response.data.code == 1) {
-                            $location.path(`/login`);
-                        } else {
-                            $scope.msg = "There was an error loading the homepage."
-                        }
-
-                    }
-                )
         }
 
         $scope.getHome();
